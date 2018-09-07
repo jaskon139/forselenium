2355e384542e2e1fb3ad1a31b02d16bb79235090
jaskon139 <jaskon139@gmail.com>
2018-09-07 17:52:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ export PATH=/app/node-v8.11.4-linux-x64/bin/:$PATH
 
 #qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp:127.0.0.1:8888-:22 -m 512 -hda /app/ssh_and_ss/tc/tc.img < /dev/null &
 
-cd /opt/wetty/ &&  /app/node-v8.11.4-linux-x64/bin/node app.js -p $PORT
+cd /opt/wetty/ &&  /app/node-v8.11.4-linux-x64/bin/npm install && /app/node-v8.11.4-linux-x64/bin/node app.js -p $PORT
