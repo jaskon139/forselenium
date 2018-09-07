995907b3112a6c52faf76ed49baa2bd7c8a1a81a
jaskon139 <jaskon139@gmail.com>
2018-09-07 14:30:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-qemu-system-x86_64 -nographic -net nic,vlan=0 -net user -m 512 -hda /app/ssh_and_ss/tc/tc.img < /dev/null &
+qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp:127.0.0.1:8888-:22 -m 512 -hda /app/ssh_and_ss/tc/tc.img < /dev/null &
 
-cd /app && npm install && node app.js -p 3000
+cd /app && npm install && node app.js -p $PORT
